f168e0e6971857445bc477d9a2571098851cf351
Manju <manjunath960650@gmail.com>
2025-11-14 16:20:34 +0530

    terraform practice

--- 01-intro.tf
+++ 01-intro.tf
@@ -5,6 +5,7 @@ resource "local_file" "foo" {
   filename = "/tmp/foo.bar"
 }
 
+#normal varibale
 variable "x" {
   default = 10
 }
@@ -12,7 +13,17 @@ variable "x" {
 output "x" {
   value = var.x
 }
-
+#if our values has only varibale then var.x, if you want to pass multple string then "${var.x}"
 output "x1" {
   value = "The value of x  is - ${var.x}"
+}
+
+#list variable
+
+variable "y" {
+  default = [1,0.2,false,"abc"]
+}
+
+output "y" {
+  value = var.y[1]
 }
