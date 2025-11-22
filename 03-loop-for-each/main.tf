f4974c3cd6c3a18a45c54a3f0ea178535f9c7dc3
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-11-22 11:04:15 +0530

    teraform code

--- 03-loop-for-each/main.tf
+++ 03-loop-for-each/main.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "test" {
   for_each = var.x
   provisioner "local-exec" {
-    command = "echo value of ${each.key} = ${each.value} "
+    command = "echo value of ${each.key} = ${each.value}"
   }
 }
 
