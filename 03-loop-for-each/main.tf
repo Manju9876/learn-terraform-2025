7c82ec6a1b753ef1932e7cba2832ff852298ff0e
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-11-22 10:52:44 +0530

    teraform code

--- 03-loop-for-each/main.tf
+++ 03-loop-for-each/main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    null = {
-      source  = "hashicorp/null"
-      version = "3.2.4"
-    }
-  }
-}
 resource "null_resource" "test" {
   for_each = var.x
   provisioner "local-exec" {
