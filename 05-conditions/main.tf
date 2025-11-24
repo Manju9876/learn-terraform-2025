f7f0c520e5d68c3649332c3b337ded8de3b1e305
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-11-24 20:10:46 +0530

    teraform code

--- 05-conditions/main.tf
+++ 05-conditions/main.tf
@@ -1,4 +1,11 @@
-
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.4"
+    }
+  }
+}
 resource "local_file" "test" {
   filename = "/tmp/a.txt"
   content = var.x == "10" ? "the values is 10" : var.x
@@ -6,4 +13,8 @@ resource "local_file" "test" {
 
 variable "x" {
   default = 10
+}
+
+resource "null_resource" "tst" {
+  count = var.x == "10" ? 0 : 1
 }
