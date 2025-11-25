bd4efe3ed52c0e7fed776edec6900d08b833b691
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-11-25 23:39:16 +0530

    teraform code

--- 05-conditions/main.tf
+++ 05-conditions/main.tf
@@ -7,13 +7,16 @@ variable "x" {
   default = 10
 }
 
-resource "null_resource" "tst" {
-  count = var.x == "10" ? 0 : 1
-}
+# resource "null_resource" "tst" {
+#   count = var.x == "10" ? 0 : 1
+# }
+#
+# variable "z" {
+#   type = number
+# }
+# output "z" {
+#   value = var.z == 1 ? 100 : (var.z == 2 ? 200 : 0)
+# }
+#
 
-variable "z" {
-  type = number
-}
-output "z" {
-  value = var.z == 1 ? 100 : (var.z == 2 ? 200 : 0)
-}
+
