540d8ec04269d5672b730955b516843af0c78d04
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-12-02 22:30:24 +0530

    teraform code

--- 06-vault-secrets/main.tf
+++ 06-vault-secrets/main.tf
@@ -1,11 +1,4 @@
-terraform {
-  required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = "2.5.3"
-    }
-  }
-}
+
 provider "vault" {
   address = "http://vault.devopsbymanju.shop:8200"
   token = var.vault_token
@@ -18,5 +11,5 @@ data "vault_generic_secret" "secret_data" {
 
 resource "local_file" "sample" {
   filename = "/tmp/sample.txt"
-  content = data.vault_generic_secret.secret_data["username"]
+  content = data.vault_generic_secret.secret_data.data
 }
