840a1df868b5ef7c73fbd29f168642d371cd1147
Manju9876 <manjunathb960650@gmail.com>
2026-01-01 14:39:25 +0530

    terraform practice

--- 07-demo-instance/main.tf
+++ 07-demo-instance/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "testing" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
-  vpc_security_group_ids = "sg-043ff9d2da877c20a"
+  vpc_security_group_ids = [sg-043ff9d2da877c20a]
 
   tags = {
     Name = "testing_demo_instance"
