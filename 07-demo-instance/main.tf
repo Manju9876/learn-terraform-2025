6708597ea7d7e0c39dbcb0e0fac7372092307ca0
Manju9876 <manjunathb960650@gmail.com>
2026-01-01 12:53:06 +0530

    terraform practice

---
+++ 07-demo-instance/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "testing" {
+  ami = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "testing_demo_instance"
+  }
+}
+
+resource "aws_route53_record" "dns_record" {
+  zone_id = "Z0739029GOTW9PY9KD3W"
+  name    = "test"
+  type    = "A"
+  ttl     = 30
+  records = aws_instance.testing.private_ip
+}
