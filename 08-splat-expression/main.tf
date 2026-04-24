bf8b3be52fe9041c94288ea9fc0dc726bf2b95e7
Manju9876 <manjunathb960650@gmail.com>
2026-04-24 15:03:38 +0530

    github actions practice

--- 08-splat-expression/main.tf
+++ 08-splat-expression/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "main" {
 
   ami           = "ami-0220d79f3f480ecf5"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.main.id]
+  vpc_security_group_ids = ["sg-09eda0ff78c765172"]
 }
 variable  "instance"  {
   default = {
