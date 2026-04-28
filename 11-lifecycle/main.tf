aba1f97b48be11cf9726e5dfc9499a5edacf9415
Manju9876 <manjunathb960650@gmail.com>
2026-04-28 14:59:22 +0530

    github actions practice

--- 11-lifecycle/main.tf
+++ 11-lifecycle/main.tf
@@ -1,8 +1,13 @@
 resource "aws_instance" "demo" {
   ami = var.ami_id
-  instance_type  = "t3.small"
+  instance_type  = "t3.micro"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 variable "ami_id" {
   default = "ami-0220d79f3f480ecf5"
 }
+
